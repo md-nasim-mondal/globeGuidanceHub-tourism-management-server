8cd733126a899cdd4922b7855b382edaefa47eef
Nasim <nasim.programmer24.com>
2024-04-29 21:59:23 +0600

    solved some problems after deploy and now we can delete data and update data

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-
 app.use(cors());
+const corsConfig = {
+  origin: ["http://localhost:5173/"],
+  credentials: true,
+};
+app.use(cors(corsConfig));
 app.use(express.json());
+// app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irefuhm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -24,30 +29,70 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const touristsSpotCollection = client
       .db("globeGuidanceHubDB")
       .collection("touristsSpot");
-      app.get("/touristsSpot/:email", async (req, res) => {
-        const email = req.params.email;
-        const query = {userEmail: email}
-        const cursor = touristsSpotCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
-      });
-      app.get("/touristsSpot", async (req, res) => {
-        const cursor = touristsSpotCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      });
+    app.get("/touristsSpot/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const cursor = touristsSpotCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/touristsSpot", async (req, res) => {
+      const cursor = touristsSpotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     app.post("/touristsSpot", async (req, res) => {
       const newSpot = req.body;
       console.log(newSpot);
       const result = await touristsSpotCollection.insertOne(newSpot);
       res.send(result);
     });
+    app.get("/touristSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristsSpotCollection.findOne(query);
+      res.send(result);
+    });
+    app.put("/touristSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedTouristsSpot = req.body;
+      console.log(updatedTouristsSpot);
+      const updatedTouristsSpotDoc = {
+        $set: {
+          image: updatedTouristsSpot.image,
+          touristsSpotName: updatedTouristsSpot.touristsSpotName,
+          countryName: updatedTouristsSpot.countryName,
+          location: updatedTouristsSpot.location,
+          shortDescription: updatedTouristsSpot.shortDescription,
+          averageCost: updatedTouristsSpot.averageCost,
+          seasonality: updatedTouristsSpot.seasonality,
+          travelTime: updatedTouristsSpot.travelTime,
+          totalVisitorsPerYear: updatedTouristsSpot.totalVisitorsPerYear,
+        },
+      };
+      const result = await touristsSpotCollection.updateOne(
+        filter,
+        updatedTouristsSpotDoc,
+        options
+      );
+      res.send(result);
+    });
+    app.delete("/touristSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      console.log(id, query);
+      const result = await touristsSpotCollection.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
