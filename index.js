7b4d90716b1b78081139b91a7feaa15f03754789
Nasim <nasim.programmer24.com>
2024-04-30 23:58:02 +0600

    comment deleted

--- index.js
+++ index.js
@@ -108,7 +108,6 @@ async function run() {
       const result = await touristsSpotCollection.deleteOne(query);
       res.send(result);
     });
-    // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
